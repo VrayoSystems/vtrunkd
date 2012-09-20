0dcdc9ba05286fadbdd967f7762ea0e2fba8488e
Kyznecov Andrey <andreykyz@gmail.com>
2012-09-20 12:28:02 +0400

    log_getter: ntp sync and more...

--- log_getter.sh
+++ log_getter.sh
@@ -3,6 +3,9 @@ echo "Starting..."
 echo "Clear syslog"
 ssh user@cli-32 "cat /dev/null | sudo tee /var/log/syslog"
 ssh user@srv-32 "cat /dev/null | sudo tee /var/log/syslog"
+echo "NTP sync..."
+ssh user@cli-32 "sudo ntpdate 2.ru.pool.ntp.org"
+ssh user@srv-32 "sudo ntpdate 2.ru.pool.ntp.org"
 echo "Starting server..."
 ssh user@srv-32 "sudo /home/user/sandbox/vtrunkd_test1/vtrunkd -s -f /home/user/sandbox/vtrunkd_test1/test/vtrunkd-srv.test.conf -P 5003"
 sleep 5
@@ -27,6 +30,8 @@ grep `grep " Session " /tmp/syslog-srv* | awk -F[ {'print $2'} | awk -F] {'print
 grep `grep " Session " /tmp/syslog-srv* | awk -F[ {'print $2'} | awk -F] {'print $1'} | tail -1` /tmp/syslog-srv* > /tmp/syslog-2_srv$1
 grep "First select time" /tmp/syslog-1_cli$1 > /tmp/syslog-1_cli$1_select_time
 grep "First select time" /tmp/syslog-2_cli$1 > /tmp/syslog-2_cli$1_select_time
+grep 'write_buf_add called!' /tmp/syslog-1_cli$1 > /tmp/syslog-1_cli$1_write_buf
+grep 'write_buf_add called!' /tmp/syslog-2_cli$1 > /tmp/syslog-2_cli$1_write_buf
 scp /tmp/syslog* andrey@bonanza:~/Dropbox/alarm_logs/
 rm /tmp/syslog*
 echo "Clear syslog"
