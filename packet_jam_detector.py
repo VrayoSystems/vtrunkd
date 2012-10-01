fa06f0001922e812ac34d46d65bc65bf26e6ec82
Kyznecov Andrey <andreykyz@gmail.com>
2012-10-01 17:05:39 +0400

    packet_jam_detector.py ploting to png file

--- packet_jam_detector.py
+++ packet_jam_detector.py
@@ -1,18 +1,26 @@
-import sys, time, glob, os
+import sys, time, glob, os, numpy
+import matplotlib.pyplot as plt
 def printTiming(fileName, stopTime):
         outFile = open("timing_" + fileName, 'w')
         prev = 0
+        timings = []
         for l in file(fileName):
                 dl = l.split(" ");
-                sdtime = dl[2];
-                dt = time.strptime(sdtime.split(".")[0], '%H:%M:%S');
+                sdtime = dl[3];
+                dt = time.strptime("14/09/12 %s000" % sdtime, '%d/%m/%y %H:%M:%S.%f')
                 ms = int(sdtime.split(".")[1])
                 t = int(time.mktime(dt))*1000+ms
                 if (t > stopTime):
                         break
-                if prev == 0: prev = t
+                if prev == 0:
+                        prev = t
+                timings.append(t - prev)
                 outFile.write((sdtime + " {0}").format(prev - t) + '\n')
                 prev = t
+        print fileName
+        print len(timings)
+        plt.bar(numpy.arange(len(timings)), timings)
+        plt.savefig("bar_chart_" + fileName)
         outFile.close()
 
 def findTime(fileName):
@@ -21,8 +29,8 @@ def findTime(fileName):
         for line in file(fileName):
                 if -1 != line.find("Requesting bad frame"):
                         splittedLine = line.split(" ");
-                        sdtime = splittedLine[2];
-                        splittedLine = time.strptime("14/09/12 %s000" % sdtime, '%d/%m/%y %H:%M:%S.%f');
+                        sdtime = splittedLine[3];
+                        splittedLine = time.strptime("14/09/12 %s000" % sdtime, '%d/%m/%y %H:%M:%S.%f')
                         ms = int(sdtime.split(".")[1])
                         timeMs = int(time.mktime(splittedLine))*1000+ms
                         timeLine = sdtime
