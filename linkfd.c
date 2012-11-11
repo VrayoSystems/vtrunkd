33c13f20f853f6d85ccb4718a1c4fa2416c81f32
Kyznecov Andrey <andreykyz@gmail.com>
2012-11-11 12:59:26 +0400

    fix max_of_max_speed calculation

--- linkfd.c
+++ linkfd.c
@@ -962,7 +962,7 @@ int ag_switcher() {
         if ((i != my_physical_channel_num) && (max_of_max_send_q < shm_conn_info->stats[i].max_send_q)) {
             max_of_max_send_q = shm_conn_info->stats[i].max_send_q;
         }
-        if ((i != my_physical_channel_num) && (max_of_max_speed < shm_conn_info->stats[i].max_upload_speed)) {
+        if ((max_of_max_speed < shm_conn_info->stats[i].max_upload_speed)) {
             max_of_max_speed = shm_conn_info->stats[i].max_upload_speed;
         }
     }
@@ -975,7 +975,7 @@ int ag_switcher() {
     int send_q_delta = my_max_send_q - max_of_max_send_q;
 #ifdef DEBUGG
     vtun_syslog(LOG_INFO, "logical_chanel num - %i MAX_REORDER * mss - %u mss - %u cwnd - %u send_q_c - %u send_q_m - %u",my_max_send_q_chan_num, max_reorder_byte, chan_info[my_max_send_q_chan_num]->mss, chan_info[my_max_send_q_chan_num]->cwnd, send_q_c, my_max_send_q);
-    vtun_syslog(LOG_INFO, "logical_chanel num - %i send_q_delta - %i , logic_speed %i kb/s max_of_max_send_q - %u",my_max_send_q_chan_num, send_q_delta, max_of_max_speed, max_of_max_send_q);
+    vtun_syslog(LOG_INFO, "logical_chanel num - %i send_q_delta - %i , max_logic_speed %i kb/s max_of_max_send_q - %u",my_max_send_q_chan_num, send_q_delta, max_of_max_speed, max_of_max_send_q);
     vtun_syslog(LOG_INFO, "logical_chanel num - %i last hold_mode - %i",my_max_send_q_chan_num, hold_mode);
 #endif
     vtun_syslog(LOG_INFO, "channel magic speed %u KB/s max speed - %u , port %d AG_FLOW_FACTOR - %f", chan_info[my_max_send_q_chan_num]->send / 1000, max_speed, channel_ports[max_speed_chan], AG_FLOW_FACTOR);
