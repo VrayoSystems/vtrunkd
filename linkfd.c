00c46a0eee268fc5821ac903d0332bd271dbf876
Andrew Gryaznov <realgrandrew@gmail.com>
2014-03-11 06:56:45 +0400

    fixed rsr def pos

--- linkfd.c
+++ linkfd.c
@@ -2045,6 +2045,7 @@ int lfd_linker(void)
             }
         }
         int32_t rtt_shift;
+        int32_t rsr = RSR_TOP; 
         // RSR section here
 //      if (((shm_conn_info->stats[info.process_num].max_send_q * 1000) / shm_conn_info->stats[info.process_num].rtt_phys_avg) == max_speed) {
         if (info.head_channel) {
@@ -2058,7 +2059,7 @@ int lfd_linker(void)
             info.send_q_limit = (RSR_TOP * shm_conn_info->stats[info.process_num].ACK_speed)
                     / shm_conn_info->stats[0].ACK_speed;
             //vtun_syslog(LOG_INFO, "sql %d, acs_our %d, acs_max %d", info.send_q_limit, shm_conn_info->stats[info.process_num].ACK_speed, shm_conn_info->stats[0].ACK_speed);
-            uint32_t rsr = info.send_q_limit;
+            rsr = info.send_q_limit;
             rtt_shift = (shm_conn_info->stats[info.process_num].rtt_phys_avg - shm_conn_info->stats[0].rtt_phys_avg)
                     * shm_conn_info->stats[0].ACK_speed;
             rtt_shift=0;
