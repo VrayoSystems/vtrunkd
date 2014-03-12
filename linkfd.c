cf980c367191bec0717b3c9b50ff4b86550ab3aa
Andrew Gryaznov <realgrandrew@gmail.com>
2014-03-12 18:34:49 +0400

    RSR overflow debug - 2

--- linkfd.c
+++ linkfd.c
@@ -2095,11 +2095,13 @@ int lfd_linker(void)
             rtt_shift = (shm_conn_info->stats[info.process_num].rtt_phys_avg - shm_conn_info->stats[0].rtt_phys_avg) // dt in ms..
                                         * (shm_conn_info->stats[0].ACK_speed / 1000); // convert spd from mp/s to mp/ms
             
-            vtun_syslog(LOG_INFO, "sql %"PRId32", acs_our %"PRId32", acs_max %"PRId32", rtt_shift %"PRId32"",
+            if(!info.head_channel) {
+                vtun_syslog(LOG_INFO, "sql %"PRId32", acs_our %"PRId32", acs_max %"PRId32", rtt_shift %"PRId32"",
                         info.send_q_limit,
                         shm_conn_info->stats[info.process_num].ACK_speed,
                         shm_conn_info->stats[0].ACK_speed,
                         rtt_shift);
+            }
             
             //rtt_shift=0;
             
