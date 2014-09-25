edd96267a29b4b6576613e60606521891f14a906
Andrew Gryaznov <ag@vrayo.com>
2014-09-25 10:29:15 +0400

    disabled check_consistency_free

--- linkfd.c
+++ linkfd.c
@@ -3931,12 +3931,12 @@ struct timeval cpulag;
             }
             
             
-            sem_wait(&(shm_conn_info->write_buf_sem));
-            check_result = check_consistency_free(FRAME_BUF_SIZE, info.channel_amount, shm_conn_info->write_buf, &shm_conn_info->wb_free_frames, shm_conn_info->frames_buf);
-            sem_post(&(shm_conn_info->write_buf_sem));
-            if(check_result < 0) {
-                vtun_syslog(LOG_ERR, "CHECK FAILED: write_buf broken: error %d", check_result);
-            }
+            //sem_wait(&(shm_conn_info->write_buf_sem));
+            //check_result = check_consistency_free(FRAME_BUF_SIZE, info.channel_amount, shm_conn_info->write_buf, &shm_conn_info->wb_free_frames, shm_conn_info->frames_buf);
+            //sem_post(&(shm_conn_info->write_buf_sem));
+            //if(check_result < 0) {
+            //    vtun_syslog(LOG_ERR, "CHECK FAILED: write_buf broken: error %d", check_result);
+            //}
             
                last_timing.tv_sec = info.current_time.tv_sec;
                last_timing.tv_usec = info.current_time.tv_usec;
