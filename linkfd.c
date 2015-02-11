9b50eea4619819efe49a6f50828e3d2fe4993aa7
Andrew Gryaznov <ag@vrayo.com>
2015-02-11 21:07:43 +0400

    restoring W_cubic only if it is higher than current #530

--- linkfd.c
+++ linkfd.c
@@ -4538,8 +4538,10 @@ int lfd_linker(void)
             if(info.head_channel != 0) {
                 skip++;
                 vtun_syslog(LOG_INFO, "Switching head to 0 (OFF)");
-                set_W_to(info.W_cubic_copy, 1, &loss_time);
-                set_Wu_to(info.Wu_cubic_copy);
+                if(info.send_q_limit_cubic < info.W_cubic_copy) {
+                    set_W_to(info.W_cubic_copy, 1, &loss_time);
+                    set_Wu_to(info.Wu_cubic_copy);
+                }
                 info.head_change_tv = info.current_time;
                 info.head_change_safe = 0;
             }
