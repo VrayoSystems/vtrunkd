8a2b7099852f1c30d642739f203d955984fe9ef9
Andrew Gryaznov <ag@vrayo.com>
2015-03-08 18:19:49 +0400

    added FRTT sync and more dbg #642

--- linkfd.c
+++ linkfd.c
@@ -908,6 +908,9 @@ int check_rtt_latency_drop_chan(int chan_num) {
 
     
 static inline int check_force_rtt_max_wait_time(int chan_num, int *next_token_ms) {
+    // TODO: may be sync on write_buf is required??
+    //sem_wait(&(shm_conn_info->frtt)); // TODO: how to do w/o sync??
+    sem_wait(&(shm_conn_info->write_buf_sem));
     int full_rtt = ((shm_conn_info->forced_rtt_recv > shm_conn_info->frtt_local_applied) ? shm_conn_info->forced_rtt_recv : shm_conn_info->frtt_local_applied);
     int APCS = shm_conn_info->APCS;
     int tail_idx = shm_conn_info->write_buf[chan_num].frames.rel_tail;
@@ -968,6 +971,9 @@ static inline int check_force_rtt_max_wait_time(int chan_num, int *next_token_ms
     if(shm_conn_info->tokens < 0) {
         shm_conn_info->tokens = 0;
     }
+    #ifdef FRTTDBG
+    vtun_syslog(LOG_ERR, "adding tokens: %d + %d", tokens_to_add, tokens_in_out);
+    #endif
     shm_conn_info->tokens += tokens_to_add + tokens_in_out;
     
     if(shm_conn_info->tokens > 0) { // we are not syncing so it is important not to rely on being zero
@@ -978,6 +984,8 @@ static inline int check_force_rtt_max_wait_time(int chan_num, int *next_token_ms
         *next_token_ms = ms_for_token;
         return 0;
     }
+    //sem_post(&(shm_conn_info->frtt)); // TODO: how to do w/o sync??
+    sem_post(&(shm_conn_info->write_buf_sem));
 }
 
 int DL_flag_drop_allowed_unsync_stats(uint32_t chan_mask) {
--- vtun.h
+++ vtun.h
@@ -632,6 +632,7 @@ struct conn_info {
     // char sockname[100], /* remember to init to "/tmp/" and strcpy from byte *(sockname+5) or &sockname[5]*/ // not needed due to devname
     char devname[50];
     sem_t hard_sem;
+    //sem_t frtt; // for frtt calculations and tokens
     sem_t tun_device_sem;
     struct frame_seq frames_buf[FRAME_BUF_SIZE];			// memory for write_buf
     struct frame_seq resend_frames_buf[RESEND_BUF_SIZE];	// memory for resend_buf
