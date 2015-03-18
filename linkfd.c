852a76a2f54963629f0de384f313a794b175c359
Andrew Gryaznov <ag@vrayo.com>
2015-03-18 14:51:26 +0400

    testing slower flush rate

--- linkfd.c
+++ linkfd.c
@@ -976,7 +976,7 @@ static inline int check_force_rtt_max_wait_time(int chan_num, int *next_token_ms
     }
     
     APCS = (APCS > BPCS ? APCS : BPCS);
-    APCS /= 2; // flush constantly with speed slower than input
+    APCS /= 3; // flush constantly with speed slower than input
     //if(APCS < 100) APCS = 100; // make minimal speed ~= 1MBit/s
    
     //shm_conn_info->write_speed_avg = (70 * shm_conn_info->write_speed_avg + APCS) / 80;
