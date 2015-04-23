367bc2e5dd8e1b844ad4286fd136adfdcf49aa15
Andrew Gryaznov <ag@vrayo.com>
2015-04-23 23:46:11 +0400

    increased max_network_stall to 200ms

--- linkfd.c
+++ linkfd.c
@@ -127,7 +127,7 @@ struct my_ip {
 // TODO: use mean send_q value for the following def
 #define SEND_Q_AG_ALLOWED_THRESH 25000 // depends on RSR_TOP and chan speed. TODO: refine, Q: understand if we're using more B/W than 1 chan has?
 //#define MAX_LATENCY_DROP { 0, 550000 }
-#define MAX_NETWORK_STALL { 0, 100000 } // 50ms maximum network stall
+#define MAX_NETWORK_STALL { 0, 200000 } // 50ms maximum network stall
 #define MAX_LATENCY_DROP_USEC 200000 // typ. is 204-250 upto 450 max RTO at CUBIC
 #define MAX_LATENCY_DROP_SHIFT 100 // ms. to add to forced_rtt - or use above
 //#define MAX_REORDER_LATENCY { 0, 50000 } // is rtt * 2 actually, default. ACTUALLY this should be in compliance with TCP RTO
@@ -150,9 +150,9 @@ struct my_ip {
 #define MSBL_PUSHUP_K 120
 #define MAX_STUB_JITTER 1 // maximum packet jitter that we allow on buffer to happen
 #define AGAG_MAX 150
-#define SLOW_START_MAX_RUN {3, 500000} // max slow_start runtime after idle
+#define SLOW_START_MAX_RUN {5, 500000} // max slow_start runtime after idle
 #define SLOW_START_IMMUNE  {10, 100000} // no SS allowed within this period after previous SS
-#define SLOW_START_INCINT 4 // amount of packets to increase MSBL by 1 after
+#define SLOW_START_INCINT 3 // amount of packets to increase MSBL by 1 after
 
 // PLOSS is a "probable loss" event: it occurs if PSL=1or2 for some amount of packets AND we detected probable loss (possible_seq_lost)
 // this LOSS detect method uses the fact that we never push the network with 1 or 2 packets; we always push 5+ (TODO: make sure it is true!)
--- version.h
+++ version.h
@@ -1,1 +1,1 @@
-#define VERSION "v0.1504-25-gdb8b-dirty"
+#define VERSION "v0.1504-27-g2b89-dirty"
