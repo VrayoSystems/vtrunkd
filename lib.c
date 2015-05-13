7b7e5c3918d3a3aea05bf9d87ac18f9c024f9813
Andrew Gryaznov <realgrandrew@gmail.com>
2015-05-13 19:15:15 +0300

    fixed ip struct location in lib.c for HAVE_SETPROC_TITLE deps

--- lib.c
+++ lib.c
@@ -47,23 +47,6 @@ volatile sig_atomic_t __io_canceled = 0;
 #ifndef HAVE_SETPROC_TITLE
 /* Functions to manipulate with program title */
 
-struct my_ip {
-    u_int8_t    ip_vhl;     /* header length, version */
-#define IP_V(ip)    (((ip)->ip_vhl & 0xf0) >> 4)
-#define IP_HL(ip)   ((ip)->ip_vhl & 0x0f)
-    u_int8_t    ip_tos;     /* type of service */
-    u_int16_t   ip_len;     /* total length */
-    u_int16_t   ip_id;      /* identification */
-    u_int16_t   ip_off;     /* fragment offset field */
-#define IP_DF 0x4000            /* dont fragment flag */
-#define IP_MF 0x2000            /* more fragments flag */
-#define IP_OFFMASK 0x1fff       /* mask for fragmenting bits */
-    u_int8_t    ip_ttl;     /* time to live */
-    u_int8_t    ip_p;       /* protocol */
-    u_int16_t   ip_sum;     /* checksum */
-    struct  in_addr ip_src,ip_dst;  /* source and dest address */
-};
-
 extern char **environ;
 char	*title_start;	/* start of the proc title space */
 char	*title_end;     /* end of the proc title space */
@@ -127,6 +110,24 @@ void set_title(const char *fmt, ...)
 	strcat(title_start, buf);
 }
 #endif  /* HAVE_SETPROC_TITLE */
+
+struct my_ip {
+    u_int8_t    ip_vhl;     /* header length, version */
+#define IP_V(ip)    (((ip)->ip_vhl & 0xf0) >> 4)
+#define IP_HL(ip)   ((ip)->ip_vhl & 0x0f)
+    u_int8_t    ip_tos;     /* type of service */
+    u_int16_t   ip_len;     /* total length */
+    u_int16_t   ip_id;      /* identification */
+    u_int16_t   ip_off;     /* fragment offset field */
+#define IP_DF 0x4000            /* dont fragment flag */
+#define IP_MF 0x2000            /* more fragments flag */
+#define IP_OFFMASK 0x1fff       /* mask for fragmenting bits */
+    u_int8_t    ip_ttl;     /* time to live */
+    u_int8_t    ip_p;       /* protocol */
+    u_int16_t   ip_sum;     /* checksum */
+    struct  in_addr ip_src,ip_dst;  /* source and dest address */
+};
+
 
 #define SYSLOG_DUPS 3
 char *syslog_buf[SYSLOG_DUPS];
